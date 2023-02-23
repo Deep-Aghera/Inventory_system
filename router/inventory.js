0a84b1d507ebd3ffc8bcf749ec78005f6041f68e
Deep Aghera <deep.aghera@marutitech.com>
2023-02-23 14:18:47 +0530

    file upload complete

--- router/inventory.js
+++ router/inventory.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const { copyFileSync } = require('fs');
 const router = new express.Router();
 
@@ -14,12 +15,17 @@ let arrayCompare = (requredEelemnt,toBeCheckedElement) => {
     
 }
 
-router.post('/inventory/add',(req,res) => {
+router.post('/inventory/add',fileUpload(),(req,res) => {
     let requiredItemForAdd = ["product_name","category","quantity","manufacturing_time","inventory_image"];
-    let body = Object.keys(req.body);
+    console.log("here your ",req.body.product)
+    let bodyData = JSON.parse(req.body.product)
+    let body = Object.keys(bodyData);
     let isValidate = arrayCompare(requiredItemForAdd,body)
-    let {product_name,category,quantity,manufacturing_time, inventory_image,expiry_time } = req.body;
-   
+    let {product_name,category,quantity,manufacturing_time, inventory_image,expiry_time } = bodyData;
+    
+    if(!isValidate) {
+        return res.send("Please provide required element");
+    } 
    let insetQuery = `INSERT INTO Products 
    (Product_name,Product_category,Quantity,Manufacturing_time,Product_image,Expiry_time)
    VALUES ('${product_name}','${category}','${quantity}','${manufacturing_time}','${inventory_image}','${expiry_time}');`
@@ -28,24 +34,32 @@ router.post('/inventory/add',(req,res) => {
         if(err) throw Error("somthing wront in insert");
         console.log("toMe::::",result)
     })
-    if(!isValidate) {
-        return res.send("Please provide required element");
-    } 
 
+    let inventoryFile = req.files;
+    console.log("hello inventory",inventoryFile);
+   
    
 })
 
 
-router.post('/test/inventory/add',(req,res) => {
-    let requiredItemForAdd = ["product_name","category","quantity","manufacturing_time","inventory_imag"];
-    let body = req.body
-    //let isValidate = arrayCompare(requiredItemForAdd,body)
-    console.log(typeof(new Date(body.expiry_time)))
-    let temp = new Date();
+router.post('/test/inventory/add',fileUpload(), (req,res) => {
+    // let requiredItemForAdd = ["product_name","category","quantity","manufacturing_time","inventory_imag"];
+    // let body = req.body
+    // //let isValidate = arrayCompare(requiredItemForAdd,body)
+    // console.log(typeof(new Date(body.expiry_time)))
+    // let temp = new Date();
 
-    console.log("here ",temp.toISOString().slice(0,19).replace("T",' '))
+    // console.log("here ",temp.toISOString().slice(0,19).replace("T",' '))
    
-    res.send("test inventory add")
+    // res.send("test inventory add")
+    let product  =  req.body.product
+     console.log(typeof(req.body.product));
+    // console.log(req.body.product,"\n",product)
+    console.log(JSON.parse(product))
+    // console.log(JSON.parse('{"product_name" : "monitor","category" : "it","quantity" : 12,"manufacturing_time" : "2010-04-30 07:27:39","inventory_image" :"now.jpg","expiry_time" : "2010-04-30 07:27:39"}'));
+    // console.log("sprator ",typeof(data));
+    // console.log(JSON.parse(data))
+    // res.send("ok dokey");
 })
 
 
